d667b352f6e2cf48202d82cb29392dd91940d608
reddydavinci <busisatya@gmail.com>
2020-09-17 18:22:32 +0530

    "updated 1_1"

--- 1_1.cpp
+++ 1_1.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #define fd(i,b,a) for(int i=b;i>a;i--)
 #define sorts(a) sort(a.begin(),a.end())
 #define reverses(a) reverse(a.begin(),a.end())
-int main(){
+int main2(){
     string s;
     cin>>s;
     sorts(s);
@@ -31,5 +31,24 @@ int main(){
     }
     if(flag==0)
         cout<<"True";
+    return 0;
+}//THis is using sorting that may use extra space
+//We may assume that the string contains only a-z 
+int main(){
+    string s;
+    cin>>s;
+    int num=0;
+    int f=0;
+    for(int i=0;i<s.size();i++){
+        int v=s[i]-'a';
+        if((num&(1<<v)) > 0){
+            cout<<"Nope";
+            f=1;
+            break;
+        }
+        num=num|(1<<v);
+    }
+    if(f==0)
+    cout<<"yes";
     return 0;
 }
